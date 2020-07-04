e87d84e1bfa41e7328dbce78fe54b387379e4a51
Samuel Grasse-Haroldsen <sgrasseharoldsen@gmail.com>
2020-07-04 12:07:53 -0700

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,3 +2,9 @@ require 'nokogiri'
 require 'open-uri'
 
 html = open("https://flatironschool.com/")
+doc = Nokogiri::HTML(html)
+
+puts doc.css(".title-3Kf9MY")
+headers = doc.css(".title-3Kf9MY")
+
+headers.each { |header| puts header.text unless header.text == "In the news"}
